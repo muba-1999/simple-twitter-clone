6f1e29d5ca672a194e985598e17579c0f10d73cc
mubanga <mukobam4@gmail.com>
2022-05-26 16:54:30 +0200

    added some markup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {registrations: 'registrations'}
+  devise_for :users, :controllers => { registrations: 'registrations' }
+  devise_scope :user do  
+    get '/users/sign_out' => 'devise/sessions#destroy'     
+ end
   resources :tweet_clones
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
